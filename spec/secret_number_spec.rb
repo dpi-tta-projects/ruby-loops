2f93c2dee3e96ca256fa67d863ef82c309f4e07c
Ian Heraty <heratyian@gmail.com>
2025-10-03 22:11:31 +0000

    Fix test stdin for secret number

--- spec/secret_number_spec.rb
+++ spec/secret_number_spec.rb
@@ -2,19 +2,17 @@
 RSpec.describe "secret_number.rb" do
   describe "output" do
     it "asks for a guess at least once", points: 1 do
-      output = run_script_and_capture_lines("secret_number.rb", stdin: ["5", "7"])
+      output = run_script_and_capture_lines("secret_number.rb", stdin: ["5", "7"].join("\n"))
       expect(output[0]).to eq("Guess the secret number:")
     end
 
     it "prints 'too low' for a low guess", points: 1 do
-      output = run_script_and_capture_lines("secret_number.rb", stdin: ["1", "10"])
+      output = run_script_and_capture_lines("secret_number.rb", stdin: ["-1"].join("\n"))
       expect(output).to include("That's too low.").or include("That's too high.")
     end
 
     it "prints success message when guessed correctly", points: 2 do
-
-      # TODO: fix this test
-      output = run_script_and_capture_lines("secret_number.rb", stdin: (1..10).map(&:to_s))
+      output = run_script_and_capture_lines("secret_number.rb", stdin: (1..10).map(&:to_s).join("\n"))
       expect(output).to include("You guessed the secret number! Yay!")
     end
   end
