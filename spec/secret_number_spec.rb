cd3e881d812a5eb8e578d082dce6456bce1e05fa
Ian Heraty <heratyian@gmail.com>
2025-10-03 21:45:41 +0000

    Fix stdin for a couple tests. Add todo for correct guess.

--- spec/secret_number_spec.rb
+++ spec/secret_number_spec.rb
@@ -2,21 +2,19 @@
 RSpec.describe "secret_number.rb" do
   describe "output" do
     it "asks for a guess at least once", points: 1 do
-      output = run_script_and_capture_lines("secret_number.rb", inputs: ["5", "7"])
+      output = run_script_and_capture_lines("secret_number.rb", stdin: ["5", "7"])
       expect(output[0]).to eq("Guess the secret number:")
     end
 
     it "prints 'too low' for a low guess", points: 1 do
-      output = run_script_and_capture_lines("secret_number.rb", inputs: ["1", "10"])
+      output = run_script_and_capture_lines("secret_number.rb", stdin: ["1", "10"])
       expect(output).to include("That's too low.").or include("That's too high.")
     end
 
     it "prints success message when guessed correctly", points: 2 do
-      # To make test deterministic, override secret_number in file
-      source_code = File.read("secret_number.rb").sub(/\(1\.\.10\)\.to_a\.sample/, "7")
-      File.write("secret_number.rb", source_code)
 
-      output = run_script_and_capture_lines("secret_number.rb", inputs: ["7"])
+      # TODO: fix this test
+      output = run_script_and_capture_lines("secret_number.rb", stdin: (1..10).map(&:to_s))
       expect(output).to include("You guessed the secret number! Yay!")
     end
   end
